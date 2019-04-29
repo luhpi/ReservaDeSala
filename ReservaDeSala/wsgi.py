4d11fdb1a9be67392dbef7fca6e27d2b9fc5b1a6
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-04-29 11:36:32 -0300

    aaaa now it goes

---
+++ ReservaDeSala/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ReservaDeSala project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ReservaDeSala.settings")
+
+application = get_wsgi_application()
