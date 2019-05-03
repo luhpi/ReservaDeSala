e1d3d95e8d2a3485b8642ae0a2836f1bf592411c
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-05-03 15:13:10 -0300

    now the url works

--- ReservaDeSala/urls.py
+++ ReservaDeSala/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^formulario', include('reserva.urls')),
+    url(r'^formulario/', include('reserva.urls')),
     url(r'^login', include('login.urls')),
     url(r'^apoio', include('apoio.urls'))
 ]
