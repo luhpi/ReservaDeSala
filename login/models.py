2a9aed81937cddd17aa455f3099279f4b97637c8
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-05-03 14:06:36 -0300

    now with apoio

--- login/models.py
+++ login/models.py
@@ -9,4 +9,4 @@ from django.db import models
 class User(models.Model):
     usuario = models.CharField(max_length=50)
     senha = models.CharField(max_length=50)
-    apoio = models.BooleanFieldbool()
+    apoio = models.BooleanField()
--- reserva/urls.py
+++ reserva/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [url(r'^solicitante$', views.solicitante, name='solicitante'),
                url(r'^atividade$', views.atividade, name='atividade'),
+               url(r'^apoio$', views.apoio, name='apoio'),
                url(r'^reserva$', views.reserva, name='reserva')]
