2c1b3318847ebab576cc99c18246a484483e4d85
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-05-06 09:47:37 -0300

    now with two tables for apoio

--- apoio/models.py
+++ apoio/models.py
@@ -6,10 +6,14 @@ from django.db import models
 # Create your models here.
 
 
+class Apoio(models.Model):
+    data_res_ini = models.DateTimeField('data inicial')
+    data_res_fim = models.DateTimeField('data final')
+    alunos = models.IntegerField()
+    atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)
+
+
 class Reserva(models.Model):
     sala = models.CharField(max_length=30)
     servidor = models.CharField(max_length=100)
     obs = models.CharField(max_length=500)
-    data_res_ini = models.DateTimeField('data inicial')
-    data_res_fim = models.DateTimeField('data final')
-    atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)
--- apoio/views.py
+++ apoio/views.py
@@ -2,9 +2,28 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 # from django.shortcuts import render
+from reserva.models import Solicitante, Atividade
+from .models import Reserva, Apoio
+from django.template import loader
+# from django.shortcuts import render
 
 # Create your views here.
 
 
 def apoio(request):
-    return HttpResponse('<h1> this is apoio</h1>')
+    template = loader.get_template('apoio/apoio.html')
+    aux = Apoio.objects.count()
+    ultimo_form = []
+    try:
+        ultimo_form.append(Solicitante.objects.get(id=aux))
+        ultimo_form.append(Atividade.objects.get(id=aux))
+        ultimo_form.append(Reserva.objects.get(id=aux))
+        context = {
+           'ultimo_form': ultimo_form
+        }
+        return HttpResponse(template.render(context, request))
+    except Exception as e:
+        context = {
+            'erro': e
+        }
+        return HttpResponse(template.render(context, request))
