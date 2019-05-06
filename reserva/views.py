4ed67293b4a2fb7e83c5d348f1cf0248d91f0ce7
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-05-06 10:33:10 -0300

    added logic for saving solicitante

--- reserva/views.py
+++ reserva/views.py
@@ -1,13 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.template import loader
+from forms import SolicitanteForm
+# AtividadeForm, ReservaForm
 # from django.shortcuts import render
+from .models import Solicitante
+# , Atividade
+# from apoio.models import Reserva
 
 # Create your views here.
 
 
 def solicitante(request):
-    return HttpResponse('<h1> isso é solicitante </h1>')
+    template = loader.get_template('reserva/reserva.html')
+    context = {}
+    result = ""
+    if request.method == 'POST':
+        sol = SolicitanteForm(request.POST)
+        context = {'solicitante': sol,
+                   'result': result}
+        try:
+            solicitante = Solicitante(
+                curso=sol.cleaned_data['curso'],
+                nome_sol=sol.cleaned_data['nome_sol'],
+                tel=sol.cleaned_data['tel'],
+                email=sol.cleaned_data['email'],
+            )
+            solicitante.save()
+            return HttpResponse(template.render(request, context))
+        except Exception as e:
+            context = {
+                'error': e
+            }
+            return HttpResponse(template.render(request, context))
 
 
 def atividade(request):
