9332a3cc9b3e540636fde011046a3255494d416e
Luiza Pinheiro <luiza.pinheiro2011@gmail.com>
2019-05-06 11:23:23 -0300

    redid bank

--- reserva/forms.py
+++ reserva/forms.py
@@ -1,26 +1,27 @@
+# -*- coding: utf-8 -*-
 from django import forms
 
 
 class SolicitanteForm(forms.Form):
-    curso = forms.CharField(label='Curso', max_length=100)
+    CHOICES = ['Biomedicina',
+               'Enfermagem',
+               'Fisioterapia',
+               'Fonoaudiologia',
+               'Medicina',
+               u'Nutrição',
+               'Psicologia',
+               u'Especialização',
+               u'Residência Médica',
+               u'Pós-Graduação'
+               'Outro']
+    curso = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
     nome_sol = forms.CharField(label='Nome do solicitante', max_length=100)
     tel = forms.CharField(label='Curso', max_length=20)
     email = forms.CharField(label='Curso', max_length=100)
 
 
 class AtividadeForm(forms.Form):
-    CHOICES = [('Biomedicina', 'Biomedicina'),
-               ('Enfermagem', 'Enfermagem'),
-               ('Fisioterapia', 'Fisioterapia')
-               ('Fonoaudiologia', 'Fonoaudiologia'),
-               ('Medicina', 'Medicina'),
-               ('Nutrição', 'Nutrição'),
-               ('Psicologia', 'Psicologia'),
-               ('Especialização', 'Especialização'),
-               ('Residência Médica', 'Residência Médica'),
-               ('Pós-Graduação', 'Pós-Graduação'),
-               ('Outro', 'Outro')]
-    atividade = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    atividade = forms.CharField(label='Atividade', max_length=100)
     outros = forms.CharField(max_length=100)
     data_req_ini = forms.DateField('data inicio')
 
